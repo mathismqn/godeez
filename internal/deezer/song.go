b4f69396af8389572a4384186c939672e7772105
Mathis Maquenne <124215603+mathismqn@users.noreply.github.com>
2025-08-06 20:45:27 +0200

    fix(song): handle empty SNG_CONTRIBUTORS array during unmarshal

--- internal/deezer/song.go
+++ internal/deezer/song.go
@@ -1,27 +1,42 @@
 package deezer
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/flytam/filenamify"
 )
 
+type Contributors struct {
+	MainArtists []string `json:"main_artist"`
+	Composers   []string `json:"composer"`
+	Authors     []string `json:"author"`
+}
+
+func (c *Contributors) UnmarshalJSON(data []byte) error {
+	if string(data) == "[]" {
+		*c = Contributors{}
+		return nil
+	}
+
+	type Alias Contributors
+	aux := (*Alias)(c)
+
+	return json.Unmarshal(data, aux)
+}
+
 type Song struct {
-	ID           string `json:"SNG_ID"`
-	Artist       string `json:"ART_NAME"`
-	Title        string `json:"SNG_TITLE"`
-	Version      string `json:"VERSION"`
-	Cover        string `json:"ALB_PICTURE"`
-	Contributors struct {
-		MainArtists []string `json:"main_artist"`
-		Composers   []string `json:"composer"`
-		Authors     []string `json:"author"`
-	} `json:"SNG_CONTRIBUTORS"`
-	Duration    string `json:"DURATION"`
-	Gain        string `json:"GAIN"`
-	ISRC        string `json:"ISRC"`
-	TrackNumber string `json:"TRACK_NUMBER"`
-	TrackToken  string `json:"TRACK_TOKEN"`
+	ID           string       `json:"SNG_ID"`
+	Artist       string       `json:"ART_NAME"`
+	Title        string       `json:"SNG_TITLE"`
+	Version      string       `json:"VERSION"`
+	Cover        string       `json:"ALB_PICTURE"`
+	Contributors Contributors `json:"SNG_CONTRIBUTORS"`
+	Duration     string       `json:"DURATION"`
+	Gain         string       `json:"GAIN"`
+	ISRC         string       `json:"ISRC"`
+	TrackNumber  string       `json:"TRACK_NUMBER"`
+	TrackToken   string       `json:"TRACK_TOKEN"`
 }
 
 func (s *Song) GetTitle() string {
